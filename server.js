524370cfb8831518f8625585d4fc1a28bd60c92c
Emilio Rodriguez <emiliorodriguez@gmail.com>
2015-08-12 11:27:54 +0200

    normalized port

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get('/', function(req, res){
 	});
 });
 
-var server = app.listen(80, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
 	console.log('Server listening on port ' + server.address().port);
 });
